9b36c9e4a411bf39b8da70dc6c77786f2d28f19d
unknown <doniaskima344@gmail.com>
2023-05-12 20:58:15 +0100

    conflicts :bug: fixed

--- controllers/Group.controllers.ts
+++ controllers/Group.controllers.ts
@@ -1,24 +1,40 @@
 import { Request , Response } from "express";
-import { Group } from "@models/group.model";
-import User from "@models/user.model";
 
+import {Group} from "../models/group.model"
+import User from "@models/user.model";
 
 const randomCode = (): number => Math.floor(Math.random() * (1000 - 1) + 1);
 
-
-const createGroup = async (req: Request, res: Response): Promise<Response> => {
-    const {adminId, groupName,isPublic,description}=req.body;
+const createGroup = async (req:Request,res:Response):Promise<Response>=>{
+    const {adminId,groupName,isPublic,description}= req.body;
     const user = await User.findById(adminId);
     if(user){
         const newGroup = new Group({
-            name:groupName,
-            admin:adminId,
-            groupCode:randomCode(),
+            name: groupName,
+            admin: adminId,
+            groupCode: randomCode(),
             isPublic,
             description,
-        });
-        newGroup.members.push(user._id);
-        const groupInfo = await newGroup.save();
-        user.groups.push(groupInfo._id);
+          });
+          newGroup.members.push();
+          const groupInfo = await newGroup.save();
+          user.groups.push(groupInfo._id);
+          await user.save();
+          return res.json({
+            status:true,
+            message:"Group Created",
+            group:groupInfo,
+          })
     }
+      return res.json({ status: false, message: "user not found" });
+
 }
+
+const fetchAllPublicGroups = (req:Request, res:Response):void=>{
+    Group.find({isPublic:true},"name _id description").then((groups)=>{
+        return res.json({status:true,groups})
+    }).catch((err)=>{
+        console.log(err);
+        return res.json({status:false, message: err.message});
+    });
+};
