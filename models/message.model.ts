05634201de702ecdca2d4df09151ee7e5d9c6e6b
unknown <doniaskima344@gmail.com>
2023-05-30 13:52:18 +0100

    feat(Message Model addes)

--- models/message.model.ts
+++ models/message.model.ts
@@ -1,7 +1,39 @@
-import mongoose , {model , Schema , Document} from "mongoose";
-import {omit} from "ramda";
+import mongoose, { Schema, Document } from "mongoose";
 
-
-export interface UserDocument extends Document {
-    
+interface IMessage extends Document {
+  sender: mongoose.Schema.Types.ObjectId;
+  receiver: mongoose.Schema.Types.ObjectId;
+  iv: string;
+  message: string;
+  key: string;
 }
+
+const messageSchema = new Schema<IMessage>(
+  {
+    sender: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    receiver: {
+      type: Schema.Types.ObjectId,
+      required: true,
+    },
+    iv: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+    key: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Message = mongoose.model<IMessage>("messages", messageSchema);
+
+export default Message;
